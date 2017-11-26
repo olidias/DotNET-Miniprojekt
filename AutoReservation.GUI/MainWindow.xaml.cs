8304dacaa21303467650692c73c3a5cbd31457c3
olidias <odiaslal@hsr.ch>
2017-11-26 19:10:15 +0100

    Add GUI Project

---
+++ AutoReservation.GUI/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace AutoReservation.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Reservation> Reservations { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Reservation
+    {
+        public int ReservationsNr { get; set; }
+    }
+}
