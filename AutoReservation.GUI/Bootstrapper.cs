9dec1fe4d7ef0ce916de73a3c053ed3d7b22d4f5
olidias <odiaslal@hsr.ch>
2017-12-11 13:32:58 +0100

    Add missing prism files

---
+++ AutoReservation.GUI/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Unity;
+using System.Windows;
+
+namespace AutoReservation.GUI
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
