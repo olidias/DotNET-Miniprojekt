ccc413e036d2354f9a9b385dc9a45e47a22857da
olidias <odiaslal@hsr.ch>
2017-12-12 11:39:07 +0100

    Service interface methods

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,45 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        [OperationContract]
+        List<ReservationDto> GetAllReservations();
+        [OperationContract]
+        ReservationDto GetReservation(int reservationsNr);
+        [OperationContract]
+        void InsertReservation(ReservationDto reservation);
+        [OperationContract]
+        void UpdateReservation(ReservationDto reservation);
+        [OperationContract]
+        void DeleteReservation(int reservationsNr);
+
+        [OperationContract]
+        List<KundeDto> GetAllKunden();
+        [OperationContract]
+        KundeDto GetKunde(int kundenId);
+        [OperationContract]
+        void InsertKunde(KundeDto kunde);
+        [OperationContract]
+        void UpdateKunde(KundeDto kunde);
+        [OperationContract]
+        void DeleteKunde(int kundenId);
+
+        [OperationContract]
+        List<AutoDto> GetAllAutos();
+        [OperationContract]
+        AutoDto GetAuto(int autoId);
+        [OperationContract]
+        void InsertAuto(AutoDto auto);
+        [OperationContract]
+        void UpdateAuto(AutoDto auto);
+        [OperationContract]
+        void DeleteAuto(int autoId);
+
+
     }
 }
