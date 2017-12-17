2f01ce55a869788242019b0885dba4462340851f
olidias <odiaslal@hsr.ch>
2017-12-17 17:15:37 +0100

    polishments #2

--- AutoReservation.GUI/ViewModels/NewReservationWindowViewModel.cs
+++ AutoReservation.GUI/ViewModels/NewReservationWindowViewModel.cs
@@ -1,13 +1,8 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
 using AutoReservation.GUI.Commands;
-using AutoReservation.GUI.DisplayClasses;
 using AutoReservation.GUI.Views;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace AutoReservation.GUI.ViewModels
